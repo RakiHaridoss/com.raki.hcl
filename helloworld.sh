513c5793dfbdf90c405004cfa0a2d86826439f98
RakiHaridoss <radhakrishnan.haridoss@gmail.com>
2018-11-24 18:05:40 +0800

    Update helloworld.sh

--- helloworld.sh
+++ helloworld.sh
@@ -1,1 +1,20 @@
-echo "Hello World"
+pipeline {
+    agent any 
+    stages {
+        stage('Build') { 
+            steps {
+                echo "building"
+            }
+        }
+        stage('Test') { 
+            steps {
+                echo "staging"
+            }
+        }
+        stage('Deploy') { 
+            steps {
+                echo "testing"
+            }
+        }
+    }
+}
