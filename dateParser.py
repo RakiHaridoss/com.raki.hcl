2efe3b9edcf946b1eb5f86b2d89af71ce7f16a58
RakiHaridoss <radhakrishnan.haridoss@gmail.com>
2018-12-01 10:32:06 +0800

    Update dateParser.py

--- dateParser.py
+++ dateParser.py
@@ -1,17 +1,21 @@
-from datetime import datetime
+import datetime
 
-now = datetime.now()
+now = datetime.datetime.now()
 
-mm = str(now.month)
+print
+print "Current date and time using str method of datetime object:"
+print str(now)
 
-dd = str(now.day)
+print
+print "Current date and time using instance attributes:"
+print "Current year: %d" % now.year
+print "Current month: %d" % now.month
+print "Current day: %d" % now.day
+print "Current hour: %d" % now.hour
+print "Current minute: %d" % now.minute
+print "Current second: %d" % now.second
+print "Current microsecond: %d" % now.microsecond
 
-yyyy = str(now.year)
-
-hour = str(now.hour)
-
-mi = str(now.minute)
-
-ss = str(now.second)
-
-print mm + "/" + dd + "/" + yyyy + " " + hour + ":" + mi + ":" + ss
+print
+print "Current date and time using strftime:"
+print now.strftime("%Y-%m-%d %H:%M")
